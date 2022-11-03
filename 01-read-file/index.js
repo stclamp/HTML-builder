47da70dfb78027c208bfbbbd379b0ab1014b2697
= <=>
2022-11-03 16:43:12 +0200

    feat: add solve for 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('node:path');
+
+const pathText = path.join(__dirname + '/text.txt')
+
+const stream = fs.ReadStream(pathText, 'utf8');
+
+stream.on('readable', function(){
+    let text = stream.read();
+    text ? console.log(text) : null
+});
